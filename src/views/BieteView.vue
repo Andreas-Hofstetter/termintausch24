53c040351dab427fea04ff8532121a549575961a
Andreas <andreashofstetter12@gmail.com>
2025-07-29 14:38:10 +0200

    m2

---
+++ src/views/BieteView.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- eslint-disable -->
+    <h1>Geben Sie hier Ihr Angebot ab!</h1>
+</template>
+<script>
+/* eslint-disable */
+import {} from '@/main';
+export default {
+name: 'BieteView',
+props: {
+    
+},
+mounted(){
+    
+},
+data(){
+    return{
+    imageUrl:"https://cdn.pixabay.com/photo/2023/06/21/15/07/butterfly-8079524_1280.jpg"
+    }
+},
+methods: {
+}
+}
+</script>
