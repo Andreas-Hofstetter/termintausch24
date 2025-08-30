7e5fae7c33f8aa60f2c07c40f294182578e1f55f
Andreas <andreashofstetter12@gmail.com>
2025-08-31 01:19:17 +0200

    rules adjusted, bieteView restyling, gesamtpreis logic

--- src/views/BieteView.vue
+++ src/views/BieteView.vue
@@ -1,7 +1,9 @@
 <template>
 <!-- eslint-disable -->
 <p class="msg" v-if="user!=null">Hallo, {{ user.displayName }}!</p>
-<div style="text-decoration: underline;cursor: pointer;" @click="profilErgänzen()">Profil ergänzen</div>
+<div style=" cursor: pointer;" @click="peClicked = !peClicked">
+  <span class="btn">{{ peClicked ? 'Profil ausblenden ▲' : 'Profil ergänzen ▼' }}</span>
+</div>
 <div class="Profil" v-if="peClicked">
     <br/>
     <div>Id: {{ this.user.uid }}</div>
@@ -12,26 +14,35 @@
 </div>
 <div id="bieteWrapper" v-if="user!=null">
     <p>Ich biete..</p>
-    <form><div style="display: inline-block;">Terminname: </div><input v-model="title" style="display: inline-block;"></form>
-    <br/><form><div style="display: inline-block;">Start: </div><input v-model="startDate" type="date"  style="display: inline-block;"><input  type="time" v-model="startTime" style="display: inline-block;"></form>
-    <br/><form><div style="display: inline-block;">Dauer (in Stunden): </div><input v-model="dauer" type="number" style="display: inline-block;"></form>
-    <br/><form><div style="display: inline-block;">Region (des Termins): 
-    </div><select v-model="selectedRegion" style="display: inline-block;">
+    <form><div>Terminname: </div><input v-model="title" style="display: inline-block;"></form>
+    <br/><form><div>Start: </div><input v-model="startDate" type="date"  style="display: inline-block;"><input  type="time" v-model="startTime" style="display: inline-block;"></form>
+    <br/><form><div>Dauer (in Stunden): </div><input v-model="dauer" type="number" style="display: inline-block;"></form>
+    <br/><form><div>Region: 
+    </div><select v-model="selectedRegion">
         <option v-for="region in this.regions" :key="region" :value="region">
           {{ region }}
         </option>
     </select></form>
-    <br/><form><div style="display: inline-block;">Kategorie (des Termins): 
-    </div><select v-model="selectedCategory" style="display: inline-block;">
+    <br/><form><div>Kategorie: </div><select v-model="selectedCategory">
         <option v-for="category in this.categories" :key="category" :value="category">
           {{ category }}
         </option>
     </select></form>
-    <br/><form><div style="display: inline-block;">Preis: </div><input v-model="price" type="number" style="display: inline-block;"></form>
-    <br/><form><div style="display: inline-block;">Details: </div><input v-model="more" style="display: inline-block;"></form>
+    <br/><form><div>Basispreis der Dienstleistung (€): </div><input v-model="price" type="number" style="display: inline-block;"></form>
+    <br/><form><div>Preis des Termins (€): </div><input v-model="price" type="number" style="display: inline-block;"></form>
+    <br/><form><div>Details: </div><textarea v-model="more" style="display: inline-block;" placeholder="Beschreibe dein Angebot..."></textarea></form>
     <!-- <br/><form><div style="display: inline-block;">Optionen: </div><input v-model="opt" style="display: inline-block;"></form> -->
-    <br/><form><div style="display: inline-block;">Privat (Wenn geschäftlich, Haken entfernen): </div><input type="checkbox" v-model="privat" style="display: inline-block;"></form>
-    <br/><div @click="createAngebot" class="btn">Angebot abgeben</div>
+    <br/><form><div>Privat (Wenn geschäftlich, Haken entfernen): </div><input type="checkbox" v-model="privat" style="display: inline-block;"></form>
+    <label class="checkbox-label">
+       <input type="checkbox" v-model="confirmTerms" />
+       <span>Ich bestätige, dass ich Veranstalter des Termins bin oder zur Übertragung dieses Termins berechtigt bin</span>
+     </label>
+     <label class="checkbox-label">
+       <input type="checkbox" v-model="confirmLegal" />
+       <span>Ich habe die <router-link to="/rechtliches" target="_blank">AGB</router-link> gelesen und akzeptiert</span>
+     </label>
+    <div v-if="errorMsg" class="error">{{ errorMsg }}</div>
+    <div @click="createAngebot" class="btn">Angebot abgeben</div>
 </div>
 </template>
 <script>
@@ -55,21 +66,41 @@ data(){
     dauer:  1,
     startTime:  datum[1].slice(0, 5),
     price: 0,
+    basePrice:0,
     selectedRegion: "Sonstige",
     selectedCategory: "Sonstiges",
     regions:["Sonstige","Bayreuth","Bayreuth Umland"],
     categories:["Handwerk","Mieten und Leihen","Events","Gastronomie","Sonstiges"],
     email:  "test",
     title: "test",
-    more:"Text",
+    more:"Beschreiben Sie Ihr Angebot...",
     opt:"keine",
     privat:true,
-    user:null
+    confirmTerms:false,
+    confirmLegal:false,
+    user:null,
+    errorMsg:null
     }
 },
 methods:{
     async createAngebot(){
-        console.log("creatingAngebot...")
+        if(!this.startDate || !this.startTime || !this.dauer || !this.title || !this.selectedRegion || !this.selectedCategory || this.price===null || this.price===undefined || !this.more){
+            this.errorMsg="Bitte füllen Sie alle Felder aus!"
+            return
+        }
+        if(!this.confirmTerms || !this.confirmLegal){
+            this.errorMsg="Bitte bestätigen Sie alle Bedingungen!"
+          return}
+        if(!this.user){
+            this.errorMsg="Fehler: Kein User gefunden! Bitte neu einloggen!"
+            return
+        }
+        
+        const profil={"email":this.profilE,"name":this.profilN,"telefon":this.profilT,"hauptsitz":this.profilH}
+        if(Object.values(profil).some((value) => value === null|| value === undefined || value === '')){
+          this.errorMsg="Profil unvollständig!"
+          return
+        }
         const angebot=
         {"region":this.selectedRegion,
         "startTimestamp": new Date(`${this.startDate}T${this.startTime}`),
@@ -78,26 +109,29 @@ methods:{
         "creator":this.user.uid,
         "category":this.selectedCategory,
         "price":parseFloat(this.price),
+        "basePrice":parseFloat(this.basePrice),
         "more":this.more,"privat":this.privat,"anbieter":{"name":this.profilN,"id":this.user.uid}}
-        const profil={"email":this.profilE,"name":this.profilN,"telefon":this.profilT,"hauptsitz":this.profilH}
-        if(Object.values(profil).some((value) => value === null|| value === undefined || value === '')){alert("Profil unvollständig!"); return}
+        
         const hasNullEntries = Object.values(angebot).some((value) => value === null|| value === undefined || value === '');
-            if(hasNullEntries){//TO DO: Länge Beschränken!!!
-            alert("Füllen Sie alle Felder aus und überprüfen Sie ihren Login-Status!")
-            }else if (this.title.length>27) {alert("Titel zu lange!")} else{
-                if(confirm("Angebot verbindlich abgeben?")===false){return}
-                console.log(angebot)
-                try{await writeProfil(this.user.uid,profil)//TODO: nur write, falls Änderung
-                    const ref=await writeAngebot(angebot,"angeboten")
-                    alert("\nAngebot abgegeben! Überprüfen Sie regelmäßig den Reiter 'Verkauft' (Navigationsleiste)");
-                    console.log(ref)
-                    console.log("ref",ref.id)
+        if(hasNullEntries){//TO DO: Länge Beschränken!!!
+          errorMsg="Bitte füllen Sie alle Felder aus!"
+          return
+        }
+        if (this.title.length>27) {
+          alert("Titel zu lange!")
+          return
+        } 
+        if(confirm("Angebot verbindlich abgeben?")===false){return}
+        console.log(angebot)
+        try{await writeProfil(this.user.uid,profil)//TODO: nur write, falls Änderung
+            const ref=await writeAngebot(angebot,"angeboten")
+            alert("\nAngebot abgegeben! Überprüfen Sie regelmäßig den Reiter 'Verkauft' (Navigationsleiste)");
+            console.log(ref)
+            console.log("ref",ref.id)
                 
             }catch(e){alert(e)}
-            }
-    },profilErgänzen(){
-        this.peClicked=true
-    }
+            
+    },
 },async beforeMount(){
     // onMount falsch! wie kann man funktion vor render aufrufen?
     const auth = getAuth();
@@ -114,3 +148,17 @@ methods:{
         this.profilH=savedProfile.hauptsitz,this.profilT=savedProfile.telefon}
 }}
 </script>
+<style scoped>
+#bieteWrapper{
+    border-radius: 10px;
+    padding: 10px;
+    background-color: var(--bg);
+}
+#bieteWrapper .btn {
+  display: block;
+  margin: 5px auto 5px auto;
+}
+#bieteWrapper >form> div{
+  display: inline-block;
+}
+</style>
